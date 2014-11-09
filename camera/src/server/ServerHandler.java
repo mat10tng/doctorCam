cb00c0fbf1f4adafa993f0ecefc2649e1e25eb3b
mat10tng <mat10tng@gmail.com>
2014-11-09 20:19:18 +0100

    s

--- camera/src/server/ServerHandler.java
+++ camera/src/server/ServerHandler.java
@@ -9,9 +9,19 @@ public class ServerHandler extends Thread{
 	}
 	public void run(){
 		while(!Thread.interrupted()){
-			ServerReceiver sr = new ServerReceiver(1234);
-			ServerSender ss = new ServerSender();
-			ServerMonitor sm = new ServerMonitor(ss,sr);
+			ServerReceiver sr;
+			try {
+				sr = new ServerReceiver(1234);
+				ServerSender ss = new ServerSender();
+				ServerMonitor sm = new ServerMonitor(ss,sr);
+				wait();
+				ss.close();
+				sr.close();
+			} catch (IOException | InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 	}
 }
--- camera/src/server/ServerMonitor.java
+++ camera/src/server/ServerMonitor.java
@@ -1,7 +1,14 @@
 package server;
 
 public class ServerMonitor {
-
+	private boolean closeRequested; //If true, the connection should be closed.
+	private static final int AUTO = 0;
+	private static final int FORCED_IDLE = 1;
+	private static final int FORCED_MOVIE = 2;
+	private int currentFrequency;
+	private byte[] lastPicture;
+	private boolean sendPicture;
+	boolean sendMotionDetected;
 	public ServerMonitor(ServerSender ss, ServerReceiver sr) {
 		// TODO Auto-generated constructor stub
 	}
