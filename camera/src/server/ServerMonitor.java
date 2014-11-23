161e7f9e7d056562b0b8afc7e428293b4cabf2bf
Arraylist <dat11nla@student.lu.se>
2014-11-23 13:18:56 +0100

    Added comments to serverMonitor

--- camera/src/server/ServerMonitor.java
+++ camera/src/server/ServerMonitor.java
@@ -5,10 +5,11 @@ public class ServerMonitor {
 	private static final int AUTO = 0;
 	private static final int FORCED_IDLE = 1;
 	private static final int FORCED_MOVIE = 2;
-	private int currentFrequency;
-	private byte[] lastPicture;
-	private boolean sendPicture;
-	boolean sendMotionDetected;
+	private int currentFrequency; // Depends on clientMode as well 
+									//as if motion is detected by theCamera H.W.
+	private byte[] lastPicture; // The most recent picture from Camera H.W.
+	private boolean sendPicture; // Depends on currentFrequency and the time.
+	boolean sendMotionDetected; // Depends on Camera H.W. and clientMode.
 	public ServerMonitor() {
 		// TODO Auto-generated constructor stub
 	}
