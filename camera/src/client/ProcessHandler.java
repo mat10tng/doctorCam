aab1368e6e24b62519e9b48960c5aedab39ad561
ShanSenanayake <senanayake92@gmail.com>
2014-12-02 18:54:51 +0100

    ProcessHandler added createing views

--- camera/src/client/ProcessHandler.java
+++ camera/src/client/ProcessHandler.java
@@ -3,18 +3,37 @@ package client;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.HashMap;
 
+import GUIView.ViewHandler;
+import GUIView.ViewWindow;
+
+/**
+ * @author Shan
+ *
+ */
 public class ProcessHandler extends Thread {
-	private ClientMonitor monitor;
-	private ConnectionData cdata;
-	private Socket clientSockets[];
 
-	/*
+	private ClientMonitor MainMonitor;
+	private PictureMonitor picMonitor;
+	private HashMap<Integer, Socket> clientSockets;
+	private HashMap<Integer, ViewHandler> views;
+	private ConnectionData cdata;
+
+	/**
 	 * Creates a ProcessHandler thread which handles connections processes
+	 * 
+	 * @param MainMonitor
+	 *            : The main monitor which gives out connection data to the
+	 *            processhandler
+	 * @param picMonitor
+	 *            : Monitor used to create ViewHandler threads.
 	 */
-	public ProcessHandler(ClientMonitor monitor) {
-		this.monitor = monitor;
-		clientSockets = new Socket[2];
+	public ProcessHandler(ClientMonitor MainMonitor, PictureMonitor picMonitor) {
+		this.MainMonitor = MainMonitor;
+		this.picMonitor = picMonitor;
+		clientSockets = new HashMap<Integer, Socket>();
+		views = new HashMap<Integer, ViewHandler>();
 	}
 
 	/*
@@ -27,20 +46,23 @@ public class ProcessHandler extends Thread {
 	public void run() {
 		try {
 			while (!isInterrupted()) {
-				cdata = monitor.getConnectionData();
+				cdata = MainMonitor.getConnectionData();
 				Thread.currentThread().interrupt();
 				int id = cdata.getID();
 				switch (cdata.getAction()) {
 				case (ConnectionData.OPEN_CONNECTION):
-					clientSockets[id] = new Socket(cdata.getIP(),
-							cdata.getPort());
-					new ClientReceiver(monitor,
-							clientSockets[id].getInputStream(), id);
-					new ClientSender(monitor,
-							clientSockets[id].getOutputStream(), id);
+					views.put(id, new ViewHandler(picMonitor, new ViewWindow(
+							"Camera: " + id), id));
+					clientSockets.put(id,
+							new Socket(cdata.getIP(), cdata.getPort()));
+					new ClientReceiver(MainMonitor, clientSockets.get(id)
+							.getInputStream(), id);
+					new ClientSender(MainMonitor, clientSockets.get(id)
+							.getOutputStream(), id);
 					break;
 				case (ConnectionData.CLOSE_CONNECTION):
-					clientSockets[id].close();
+					views.get(id).interrupt();
+					clientSockets.get(id).close();
 					break;
 				default:
 					System.out.println("You should not be here!");
