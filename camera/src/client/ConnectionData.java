c2203a4baf68ecfa7d0b12bd1c2db2662cd4021f
TabTabTab <hansjohannyholm@gmail.com>
2014-12-03 17:04:29 +0100

    Changed connectionData

--- camera/src/client/ConnectionData.java
+++ camera/src/client/ConnectionData.java
@@ -2,29 +2,44 @@ package client;
 
 
 public class ConnectionData {
-	public static final int CLOSE_CONNECTION=0;
-	public static final int OPEN_CONNECTION=1;
-	
 	private String IP;
 	private int port;
 	private int ID;
 	private int action;
 	
+	/**
+	 * @param IP: the IP address
+	 * @param port: the port number
+	 * @param ID: an ID, usually [0,2, ..]
+	 * @param action: either A constant found in Constants.ConnectionActions
+	 */
 	public ConnectionData(String IP,int port,int ID,int action){
 		this.IP=IP;
 		this.port=port;
 		this.ID=ID;
 		this.action=action;
 	}
+	/**
+	 * @return the IP address in string format
+	 */
 	public String getIP(){
 		return IP;
 	}
+	/**
+	 * @return: the port number
+	 */
 	public  int getPort(){
 		return port;
 	}
+	/**
+	 * @return the connection ID
+	 */
 	public int getID(){
 		return ID;
 	}
+	/**
+	 * @return the action, found in Constants.ConnectionActions
+	 */
 	public  int getAction(){
 		return action;
 	}
