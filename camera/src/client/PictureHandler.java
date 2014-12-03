e192eae736113467785d7ba8bdb00f71d40ce712
TabTabTab <hansjohannyholm@gmail.com>
2014-12-03 17:32:08 +0100

    Added methods to Picturehandler

--- camera/src/client/PictureHandler.java
+++ camera/src/client/PictureHandler.java
@@ -1,12 +1,22 @@
 package client;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class PictureHandler extends Thread{
-	
-	public PictureHandler() {
-		// TODO Auto-generated constructor stub
+	private PictureMonitor pictureMonitor;
+	private HashMap<Integer,LinkedList<Picture>> pictures;
+	public PictureHandler(PictureMonitor pictureMonitor) {
+		this.pictureMonitor=pictureMonitor;
+		pictures=new HashMap<Integer,LinkedList<Picture>>();
 		
 	}
-	
+	public void registerID(int ID){
+		pictures.put(ID,new LinkedList<Picture>());
+	}
+	public void unregisterID(int ID){
+		pictures.remove(ID);
+	}
 	
 	
 	
