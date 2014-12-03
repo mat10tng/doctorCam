b24fe909a9c9461b7b5d6b05f08d2c3f72118cda
ShanSenanayake <senanayake92@gmail.com>
2014-12-03 17:52:45 +0100

    Added testinfo (that does not work)

--- camera/src/GUIControl/CameraPanel.java
+++ camera/src/GUIControl/CameraPanel.java
@@ -5,13 +5,16 @@ import java.awt.GridLayout;
 import javax.swing.JPanel;
 
 import client.ClientMonitor;
+import client.Constants;
 
 public class CameraPanel extends JPanel {
 
 	public CameraPanel(ClientMonitor monitor) {
-		super(new GridLayout(1,2));
-		this.add(new CameraButton("cam 1", "tempip1",2001,1, monitor));
-		this.add(new CameraButton("cam 2","tempip2",2002,2, monitor));
+		super(new GridLayout(1, 2));
+		this.add(new CameraButton("cam 1", Constants.DebugIps.IP1,
+				Constants.DebugIps.PORT1, Constants.DebugIps.ID1, monitor));
+		this.add(new CameraButton("cam 2", Constants.DebugIps.IP2,
+				Constants.DebugIps.PORT2, Constants.DebugIps.ID2, monitor));
 	}
 
 }
--- camera/src/client/Constants.java
+++ camera/src/client/Constants.java
@@ -1,18 +1,29 @@
 package client;
 
 public class Constants {
-	public class CameraMode{
-		public static final int IDLE_MODE=0;
-		public static final int MOVIE_MODE=1;
-		public static final int AUTO_MODE=2;
+	public class CameraMode {
+		public static final int IDLE_MODE = 0;
+		public static final int MOVIE_MODE = 1;
+		public static final int AUTO_MODE = 2;
 	}
-	public class ViewMode{
-		public static final int SYNC_MODE=0;
-		public static final int AUTO_MODE=1;
-		public static final int ASYNC_MODE=2;
+
+	public class ViewMode {
+		public static final int SYNC_MODE = 0;
+		public static final int AUTO_MODE = 1;
+		public static final int ASYNC_MODE = 2;
 	}
-	public class ConnectionActions{
-		public static final int CLOSE_CONNECTION=0;
-		public static final int OPEN_CONNECTION=1;
+
+	public class ConnectionActions {
+		public static final int CLOSE_CONNECTION = 0;
+		public static final int OPEN_CONNECTION = 1;
+	}
+
+	public class DebugIps {
+		public static final String IP1 = "testip1";
+		public static final int PORT1 = 2001;
+		public static final int ID1 = 1;
+		public static final String IP2 = "testip2";
+		public static final int PORT2 = 2002;
+		public static final int ID2 = 2;
 	}
 }
