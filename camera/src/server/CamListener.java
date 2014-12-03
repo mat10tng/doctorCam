4dabd4bd58fc8f1c99f8b2b90ac4606bf654afe9
arraylist <dat11nla@student.lu.se>
2014-12-04 00:43:54 +0100

    CamListener, half done, no frequency added to camera yet.

--- camera/src/server/CamListener.java
+++ camera/src/server/CamListener.java
@@ -1,12 +1,13 @@
 package server;
 
 import se.lth.cs.eda040.fakecamera.*;
+
 /**
- * Waits for the Camera H.W. to generate a picture. 
- * When a new picture exists it is fetched and Camera H.W. is checked for motion. 
- * The data is then handed to the monitor.
+ * Waits for the Camera H.W. to generate a picture. When a new picture exists it
+ * is fetched and Camera H.W. is checked for motion. The data is then handed to
+ * the monitor.
  * 
- *
+ * 
  */
 
 public class CamListener implements Runnable {
@@ -17,10 +18,15 @@ public class CamListener implements Runnable {
 		AxisM3006V camera = new AxisM3006V();
 		camera.init();
 		camera.connect();
-		for (int i = 0; i < 100; i++){
+		for (int i = 0; i < 100; i++) {
 			byte[] jpeg = new byte[AxisM3006V.IMAGE_BUFFER_SIZE];
-		}
+			camera.getJPEG(jpeg, 0);
+			if (camera.motionDetected()) {
+				sm.setMode(2);
+				notifyAll();
+			} else {
+				sm.setMode(1);
+			}
 		}
 	}
-
-
+}
