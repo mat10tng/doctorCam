3a3e2d6c3e1b544a96347205deec6b09ee29fae0
arraylist <dat11nla@student.lu.se>
2014-12-04 00:33:31 +0100

    Camera initialized and byte array created for every single picture

--- camera/src/server/CamListener.java
+++ camera/src/server/CamListener.java
@@ -1,5 +1,6 @@
 package server;
 
+import se.lth.cs.eda040.fakecamera.*;
 /**
  * Waits for the Camera H.W. to generate a picture. 
  * When a new picture exists it is fetched and Camera H.W. is checked for motion. 
@@ -8,6 +9,18 @@ package server;
  *
  */
 
-public class CamListener {
+public class CamListener implements Runnable {
+	private ServerMonitor sm;
 
-}
+	@Override
+	public void run() {
+		AxisM3006V camera = new AxisM3006V();
+		camera.init();
+		camera.connect();
+		for (int i = 0; i < 100; i++){
+			byte[] jpeg = new byte[AxisM3006V.IMAGE_BUFFER_SIZE];
+		}
+		}
+	}
+
+
