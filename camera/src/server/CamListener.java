7749b5dc5aa5431639477a71c2d093cac0ce6734
arraylist <dat11nla@student.lu.se>
2014-12-04 00:08:06 +0100

    Added comments to CamListener

--- camera/src/server/CamListener.java
+++ camera/src/server/CamListener.java
@@ -1,5 +1,13 @@
 package server;
 
+/**
+ * Waits for the Camera H.W. to generate a picture. 
+ * When a new picture exists it is fetched and Camera H.W. is checked for motion. 
+ * The data is then handed to the monitor.
+ * 
+ *
+ */
+
 public class CamListener {
 
 }
