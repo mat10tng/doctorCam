462abfad6908db0baf6293de49057f71c72ba3fb
mat10tng <mat10tng@gmail.com>
2014-12-06 20:32:59 -0800

    sdd

--- camera/src/GUIControl/ButtonGroupPanel.java
+++ camera/src/GUIControl/ButtonGroupPanel.java
@@ -14,18 +14,10 @@ public class ButtonGroupPanel extends JPanel {
 
 	public ButtonGroupPanel(AbstractButton button1, AbstractButton button2, AbstractButton button3, AbstractButton button4) {
 		super(new GridLayout(4, 1));
-		if (button1 != null) {
 			this.add(button1);
-		}
-		if (button2 != null) {
 			this.add(button2);
-		}
-		if(button3 != null){
 			this.add(button3);
-		}
-		if(button4 != null){
 			this.add(button4);
-		}
 	}
 
 }
